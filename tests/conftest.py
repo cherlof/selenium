70984135a1538376dca53fa10188289df34a576a
Honta <Hontasher@gmail.com>
2025-05-20 16:06:51 +0300

    w

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,9 +19,6 @@ def driver(request):
         options = webdriver.ChromeOptions()
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--remote-debugging-port=9222")
         _driver = webdriver.Chrome(options=options)
 
     elif browser_name == "firefox":
